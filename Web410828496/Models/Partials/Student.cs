9315a8f509d3e28922aebd0a821da90c1c741f01
liang-liang-la <connie11607@yahoo.com>
2022-06-09 20:14:19 +0800

    0609

--- Web410828496/Models/Partials/Student.cs
+++ Web410828496/Models/Partials/Student.cs
@@ -18,16 +18,20 @@ namespace Web410828496.Models
         [Required(ErrorMessage = "請填寫姓名")]
         [Display(Name = "姓名")]
         [StringLength(5, MinimumLength = 2, ErrorMessage = "請輸入2-5個字")]
-        public string Name { get; set; }
+        public string Username { get; set; }
 
         [Required(ErrorMessage = "請填寫學號")]
         [Display(Name = "學號")]
-        [StringLength(10, ErrorMessage = "請輸入至少10個字")]
+        [StringLength(10, ErrorMessage = "請輸入10個字以內")]
         public string Number { get; set; }
 
         [Required(ErrorMessage = "請填寫信箱")]
         [Display(Name = "信箱")]
         [EmailAddress]
         public string Email { get; set; }
+
+        [Required(ErrorMessage ="必填欄位")]
+        [Display(Name="是否註冊")]
+        public bool IsActive { get; set; }
     }
 }
